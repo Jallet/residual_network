375304b35adb9df0d2f17f51c3492afcd3c87715
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-05 15:33:35 +0800

    generalize net.py, input network as a param

--- src/net.py
+++ src/net.py
@@ -3,24 +3,37 @@ caffe_root = '/home/jiangliang/code/caffe/'
 import sys
 sys.path.insert(0, caffe_root + 'python')
 import caffe
+import argparse
 
-net = caffe.Net('models/train_val_fuse.prototxt', caffe.TRAIN)
-print 'shape of data:'
-total_size = 0
-for k, v in net.blobs.items():
-    size = 1
-    for i in range(len(v.data.shape)):
-        size = size * v.data.shape[i]
-    print("{}, {}, ({})".format(k, v.data.shape, size))
-    total_size = total_size + size
-print("total_size: {}".format(total_size))
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("network", help = 'network to test')
+    return parser
 
-print 'shape of params: '
-param_num = 0
-for k, v in net.params.items():
-    num = 1
-    for i in range(len(v[0].data.shape)):
-        num = num * v[0].data.shape[i]
-    print("{}, {}, ({})".format(k, v[0].data.shape, num))
-    param_num = param_num + num
-print "num of params: {}".format(param_num)
+def main():
+    parser = argparser()
+    args = parser.parse_args()
+    network = args.network
+    net = caffe.Net(network, caffe.TRAIN)
+    print 'shape of data:'
+    total_size = 0
+    for k, v in net.blobs.items():
+        size = 1
+        for i in range(len(v.data.shape)):
+            size = size * v.data.shape[i]
+        print("{}, {}, ({})".format(k, v.data.shape, size))
+        total_size = total_size + size
+    print("total_size: {}".format(total_size))
+    
+    print 'shape of params: '
+    param_num = 0
+    for k, v in net.params.items():
+        num = 1
+        for i in range(len(v[0].data.shape)):
+            num = num * v[0].data.shape[i]
+        print("{}, {}, ({})".format(k, v[0].data.shape, num))
+        param_num = param_num + num
+    print "num of params: {}".format(param_num)
+    
+if __name__ == "__main__":
+    main()
