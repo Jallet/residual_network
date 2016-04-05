cb4773252c2d59cada547747eac0dfc8d7e87102
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-05 19:02:13 +0800

    scripts to quickly solve ResNet-cifar

---
+++ src/solve_rn_cifar.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+    /home/liangjiang/code/residual_network/src/solve_network.py /home/liangjiang/code/residual_network/prototxt/ResNet-cifar-solver.prototxt -l /home/liangjiang/code/residual_network/results/loss/ResNet-cifar/loss --max_iter 20000 
