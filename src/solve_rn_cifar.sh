dff78d705ba3292b0056b8bcecec6ff3941d96ad
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-06 14:17:35 +0800

    add clean and create directory code

--- src/solve_rn_cifar.sh
+++ src/solve_rn_cifar.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
-    /home/liangjiang/code/residual_network/src/solve_network.py /home/liangjiang/code/residual_network/prototxt/ResNet-cifar-solver.prototxt -l /home/liangjiang/code/residual_network/results/loss/ResNet-cifar/loss --max_iter 20000 
+#clean
+rm -r /home/liangjiang/code/residual_network/results/loss/ResNet-cifar
+rm -r /home/liangjiang/code/residual_network/results/snapshots/ResNet-cifar
+mkdir -p /home/liangjiang/code/residual_network/results/loss/ResNet-cifar
+mkdir -p  /home/liangjiang/code/residual_network/results/snapshots/ResNet-cifar
+
+/home/liangjiang/code/residual_network/src/solve_network.py /home/liangjiang/code/residual_network/prototxt/ResNet-cifar-solver.prototxt -l /home/liangjiang/code/residual_network/results/loss/ResNet-cifar/loss --max_iter 20000 
