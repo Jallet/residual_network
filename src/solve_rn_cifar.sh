578fb00937f6190f48409a6ff1025eabe361fcac
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-07 15:54:59 +0800

    [DAIRY][loss=0.01]set weight decay = 0.001, to prevent overfit, much better

--- src/solve_rn_cifar.sh
+++ src/solve_rn_cifar.sh
@@ -6,4 +6,4 @@ mkdir -p /home/liangjiang/code/residual_network/results/loss/ResNet-cifar
 mkdir -p /home/liangjiang/code/residual_network/results/accuracy/ResNet-cifar
 mkdir -p  /home/liangjiang/code/residual_network/results/snapshots/ResNet-cifar
 
-/home/liangjiang/code/residual_network/src/solve_network.py /home/liangjiang/code/residual_network/prototxt/ResNet-cifar-solver.prototxt -l /home/liangjiang/code/residual_network/results/loss/ResNet-cifar/ResNet-cifar-loss -a /home/liangjiang/code/residual_network/results/accuracy/ResNet-cifar/ResNet-cifar-acc --max_iter 20000  --record_iter 1
+/home/liangjiang/code/residual_network/src/solve_network.py /home/liangjiang/code/residual_network/prototxt/ResNet-cifar-solver.prototxt -l /home/liangjiang/code/residual_network/results/loss/ResNet-cifar/ResNet-cifar-loss -a /home/liangjiang/code/residual_network/results/accuracy/ResNet-cifar/ResNet-cifar-acc --max_iter 60000  --record_iter 1
