63c5225b1908aa69dec91d14bd90feb798cb96d3
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-08 00:01:09 +0800

    add val-loss val-acc

--- src/solve_dn_cifar2.sh
+++ src/solve_dn_cifar2.sh
@@ -6,4 +6,4 @@ mkdir -p /home/liangjiang/code/residual_network/results/loss/DirNet-cifar2
 mkdir -p  /home/liangjiang/code/residual_network/results/accuracy/DirNet-cifar2
 mkdir -p  /home/liangjiang/code/residual_network/results/snapshots/DirNet-cifar2
 
-/home/liangjiang/code/residual_network/src/solve_network.py /home/liangjiang/code/residual_network/prototxt/DirNet-cifar2-solver.prototxt -l /home/liangjiang/code/residual_network/results/loss/DirNet-cifar2/DirNet-cifar-loss -a /home/liangjiang/code/residual_network/results/accuracy/DirNet-cifar2/DirNet-cifar-acc --max_iter 20000 
+/home/liangjiang/code/residual_network/src/solve_network.py /home/liangjiang/code/residual_network/prototxt/DirNet-cifar2-solver.prototxt --train-loss /home/liangjiang/code/residual_network/results/loss/DirNet-cifar2/DirNet-cifar-train-loss --train-acc /home/liangjiang/code/residual_network/results/accuracy/DirNet-cifar2/DirNet-cifar-train-acc --val-loss /home/liangjiang/code/residual_network/results/loss/DirNet-cifar2/DirNet-cifar-val-loss --val-acc /home/liangjiang/code/residual_network/results/accuracy/DirNet-cifar2/DirNet-cifar-val-acc --max_iter 20000 
