e0990702fc17c11cbd541683dfac03b30c9026bc
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-10 16:23:25 +0800

    add trainloss, valloss, trainacc, valacc

--- src/solve.py
+++ src/solve.py
@@ -58,7 +58,7 @@ snapshots/DirNet-cifar{}".format(i), shell = True)
         child.wait()
 
 def main():
-    max_iter = 6000
+    max_iter = 20
     test_iter = 200
     record_iter = 1
     threshold = -100
@@ -91,23 +91,35 @@ accuracy/DirNet-cifar"
     min_loss = 0
     total_loss = []
     total_acc = []
-    total_loss_path = "/home/liangjiang/\
+    total_train_loss_path = "/home/liangjiang/\
 code/residual_network/results/\
-loss/DirNet-cifar/DirNet-cifar-loss"
-    total_acc_path = "/home/liangjiang/\
+loss/DirNet-cifar/DirNet-cifar-loss.train"
+    total_train_acc_path = "/home/liangjiang/\
 code/residual_network/results/\
-accuracy/DirNet-cifar/DirNet-cifar-acc"
+accuracy/DirNet-cifar/DirNet-cifar-acc.train"
+    total_val_loss_path = "/home/liangjiang/\
+code/residual_network/results/\
+loss/DirNet-cifar/DirNet-cifar-loss.val"
+    total_val_acc_path = "/home/liangjiang/\
+code/residual_network/results/\
+accuracy/DirNet-cifar/DirNet-cifar-acc.val"
     print("cmd: {}".format(cmd))
     for i in range(net_num):
         print i
         loss_path = "{}{}/DirNet-cifar-loss".format(loss_prefix, i)
+        train_loss = loss_path + ".train"
+        val_loss = loss_path + ".val"
         acc_path = "{}{}/DirNet-cifar-acc".format(acc_prefix, i)
+        train_acc = acc_path + ".train"
+        val_acc = acc_path + ".val"
         execute_cmd = ""
         if i == 0:
             execute_cmd = cmd \
             + " " + solver_prefix + str(i) + "-solver.prototxt" \
-            + " --loss " + loss_path \
-            + " --accuracy " + acc_path \
+            + " --train-loss " + train_loss \
+            + " --val-loss " + val_loss \
+            + " --train-acc " + train_acc \
+            + " --val-acc " + val_acc \
             + " --threshold " + str(threshold) \
             + " --max_iter " + str(max_iter) \
             + " --record_iter " + str(record_iter) \
@@ -118,8 +130,10 @@ accuracy/DirNet-cifar/DirNet-cifar-acc"
                      + str(i - 1) + "/*.caffemodel"
             execute_cmd = cmd \
             + " " + solver_prefix + str(i) + "-solver.prototxt" \
-            + " --loss " + loss_path \
-            + " --accuracy " + acc_path \
+            + " --train-loss " + train_loss \
+            + " --val-loss " + val_loss \
+            + " --train-acc " + train_acc \
+            + " --val-acc " + val_acc \
             + " --snapshot " + snapshot \
             + " --threshold " + str(threshold) \
             + " --max_iter " + str(max_iter)\
@@ -135,17 +149,23 @@ accuracy/DirNet-cifar/DirNet-cifar-acc"
         except:
             print "Subprocess Error"
             sys.exit()
-        preloss = np.loadtxt(loss_path)
-        preacc = np.loadtxt(acc_path)
-        total_loss = np.hstack((total_loss, preloss))
-        total_acc = np.hstack((total_acc, preacc))
+        pre_train_loss = np.loadtxt(train_loss)
+        pre_train_acc = np.loadtxt(train_acc)
+        total_train_loss = np.hstack((total_loss, pre_train_loss))
+        total_train_acc = np.hstack((total_acc, pre_train_acc))
+        pre_val_loss = np.loadtxt(val_loss)
+        pre_val_acc = np.loadtxt(val_acc)
+        total_val_loss = np.hstack((total_loss, pre_val_loss))
+        total_val_acc = np.hstack((total_acc, pre_val_acc))
         if i == 0:
-            minloss = preloss[-1]
+            minloss = pre_train_loss[-1]
         else:
-            if minloss > preloss[-1]:
-               minloss = preloss[-1]
+            if minloss > pre_train_loss[-1]:
+               minloss = pre_train_loss[-1]
         print("minloss: {}".format(minloss))
-        np.savetxt(total_loss_path, total_loss)
-        np.savetxt(total_acc_path, total_acc)
+        np.savetxt(total_train_loss_path, total_train_loss)
+        np.savetxt(total_train_acc_path, total_train_acc)
+        np.savetxt(total_val_loss_path, total_val_loss)
+        np.savetxt(total_val_acc_path, total_val_acc)
 if __name__ == '__main__':
     main()
