30c0a2cf5ba5ec1f6862b2479397616f85ff66cc
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-12 16:05:18 +0800

    add title param

--- src/show_figure.py
+++ src/show_figure.py
@@ -6,15 +6,20 @@ import argparse
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("path")
+    parser.add_argument("-t", "--title",
+            help = "title of figure", 
+            action = "store", default = "data")
     return parser
+
 parser = argparser()
 args = parser.parse_args()
 path = args.path
+title = args.title
 
 data = np.loadtxt(path)
 plt.figure(1)
 plt.plot(data)
 plt.legend()
 plt.grid(True)
-plt.title("data")
+plt.title(title)
 plt.show()
