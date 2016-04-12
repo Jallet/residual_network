098671be6e1bf3297420dbf8258a5b4b269b4e3c
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-12 16:04:50 +0800

    save loss and accuracy evary record_iter iterations

--- src/solve_network.py
+++ src/solve_network.py
@@ -47,7 +47,7 @@ def argparser():
     parser.add_argument("--record_iter", "-r",  
             help = "iterations to record loss",
             action = "store", dest = "record_iter",
-            type = int, default = 1)
+            type = int, default = 20)
     parser.add_argument("--early-stop", "-e", 
             help = "Whether stop when loss does not decrease",
             action = "store_true", dest = "early_stop")
@@ -82,32 +82,46 @@ def main():
     val_acc = np.zeros(max_iter / record_iter) 
     pre_loss = 0
     pre_acc = 0
+
+    cur_train_loss = np.zeros(record_iter)
+    cur_train_acc = np.zeros(record_iter)
+    cur_val_loss = np.zeros(record_iter)
+    cur_val_acc = np.zeros(record_iter)
+
     for i in range(max_iter):
         solver.step(1)
-        cur_train_loss = solver.net.blobs["loss"].data
-        cur_train_acc = solver.net.blobs["accuracy"].data
-        cur_val_loss = solver.test_nets[0].blobs["loss"].data
-        cur_val_acc = solver.test_nets[0].blobs["accuracy"].data
-        if i % record_iter == 0:
-            train_loss[i / record_iter] = cur_train_loss
-            train_acc[i / record_iter] = cur_train_acc
-            val_loss[i / record_iter] = cur_val_loss
-            val_acc[i / record_iter] = cur_val_acc
-        if early_stop and (i % test_iter == 0):
-            if i == 0:
-                pre_train_loss = cur_train_loss
-                pre_train_acc = cur_train_acc
-                pre_val_loss = cur_val_loss
-                pre_val_acc = cur_val_acc
-            elif (pre_train_loss - cur_train_loss) / pre_train_loss < threshold:
-                print "Converged, stopping...."
+        cur_train_loss[i % record_iter] = solver.net.blobs["loss"].data
+        cur_train_acc[i % record_iter] = solver.net.blobs["accuracy"].data
+        cur_val_loss[i % record_iter] = solver.test_nets[0].blobs["loss"].data
+        cur_val_acc[i % record_iter] = solver.test_nets[0].blobs["accuracy"].data
+        if i % record_iter == record_iter - 1:
+            train_loss[i / record_iter] = np.average(cur_train_loss)
+            train_acc[i / record_iter] = np.average(cur_train_acc)
+            val_loss[i / record_iter] = np.average(cur_val_loss)
+            val_acc[i / record_iter] = np.average(cur_val_acc)
+
+            cur_train_loss = np.zeros(record_iter)
+            cur_train_acc = np.zeros(record_iter)
+            cur_val_loss = np.zeros(record_iter)
+            cur_val_acc = np.zeros(record_iter)
+
+            train_temp_loss = train_loss[0 : i / record_iter + 1]
+            train_temp_acc = train_acc[0 : i / record_iter + 1]
+            val_temp_loss = val_loss[0 : i / record_iter + 1]
+            val_temp_acc = val_acc[0 : i / record_iter + 1]
+            np.savetxt(train_loss_path, train_temp_loss)
+            np.savetxt(train_acc_path, train_temp_acc)
+            np.savetxt(val_loss_path, val_temp_loss)
+            np.savetxt(val_acc_path, val_temp_acc)
+        #if early_stop and (i % test_iter == 0):
+        #    if i == 0:
+        #        pre_train_loss = cur_train_loss
+        #        pre_train_acc = cur_train_acc
+        #        pre_val_loss = cur_val_loss
+        #        pre_val_acc = cur_val_acc
+        #    elif (pre_train_loss - cur_train_loss) / pre_train_loss < threshold:
+        #        print "Converged, stopping...."
     solver.snapshot()
-    train_loss = train_loss[0 : i / record_iter + 1]
-    train_acc = train_acc[0 : i / record_iter + 1]
-    np.savetxt(train_loss_path, train_loss)
-    np.savetxt(train_acc_path, train_acc)
-    np.savetxt(val_loss_path, val_loss)
-    np.savetxt(val_acc_path, val_acc)
 
 if __name__ == "__main__":
     main()
