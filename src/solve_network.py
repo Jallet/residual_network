a7e61b888a19031763ea5b97e2149e80107419ee
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-26 00:03:39 +0800

    solve the output of net, add epoch argument etc.

--- src/solve_network.py
+++ src/solve_network.py
@@ -3,6 +3,7 @@ import sys
 import caffe
 import argparse
 import numpy as np
+import lmdb
 
 def argparser():
     parser = argparse.ArgumentParser()
@@ -44,9 +45,24 @@ def argparser():
             help = "iterations to record loss",
             action = "store", dest = "record_iter",
             type = int, default = 20)
-    parser.add_argument("--early-stop", "-e", 
+    parser.add_argument("--early-stop", 
             help = "Whether stop when loss does not decrease",
             action = "store_true", dest = "early_stop")
+    parser.add_argument("--epoch", "-e",
+            help = "Epoches to train",
+            action = "store", default = 1, type = int)
+    parser.add_argument("--output_layer", 
+            help = "Which layer to save as the output",
+            action = "store", default = "ip")
+    parser.add_argument("train_output", 
+            help = "Where to store the training output",
+            action = "store")
+    parser.add_argument("test_output", 
+            help = "Where to store the testing output",
+            action = "store")
+    parser.add_argument("--batch_size", "-b",
+            help = "Batch size",
+            action = "store", type = int, default = 100)
     return parser
 
 def main():
@@ -64,6 +80,14 @@ def main():
     record_iter = args.record_iter
     test_iter = args.test_iter
     early_stop = args.early_stop 
+    epoch = args.epoch
+    train_output = args.train_output 
+    test_output = args.test_output 
+    output_layer = args.output_layer
+    batch_size = args.batch_size
+    
+    train_output_lmdb = lmdb.open(train_output, map_size = int(1e12))
+    test_output_lmdb = lmdb.open(test_output, map_size = int(1e12))
 
     caffe.set_mode_gpu()
     caffe.set_device(0)
@@ -83,40 +107,69 @@ def main():
     cur_train_acc = np.zeros(record_iter)
     cur_val_loss = np.zeros(record_iter)
     cur_val_acc = np.zeros(record_iter)
+    
+    with train_output_lmdb.begin(write = True) as train_output_txn:
+        with test_output_lmdb.begin(write = True) as test_output_txn:
+            for e in range(epoch):
+                for i in range(max_iter):
+                    solver.step(1)
+                    cur_train_loss[i % record_iter] = solver.net.blobs["loss"].data
+                    cur_train_acc[i % record_iter] = solver.net.blobs["accuracy"].data
+                    cur_val_loss[i % record_iter] = solver.test_nets[0].blobs["loss"].data
+                    cur_val_acc[i % record_iter] = solver.test_nets[0].blobs["accuracy"].data
+                    if i % record_iter == record_iter - 1:
+                        train_loss[i / record_iter] = np.average(cur_train_loss)
+                        train_acc[i / record_iter] = np.average(cur_train_acc)
+                        val_loss[i / record_iter] = np.average(cur_val_loss)
+                        val_acc[i / record_iter] = np.average(cur_val_acc)
 
-    for i in range(max_iter):
-        solver.step(1)
-        cur_train_loss[i % record_iter] = solver.net.blobs["loss"].data
-        cur_train_acc[i % record_iter] = solver.net.blobs["accuracy"].data
-        cur_val_loss[i % record_iter] = solver.test_nets[0].blobs["loss"].data
-        cur_val_acc[i % record_iter] = solver.test_nets[0].blobs["accuracy"].data
-        if i % record_iter == record_iter - 1:
-            train_loss[i / record_iter] = np.average(cur_train_loss)
-            train_acc[i / record_iter] = np.average(cur_train_acc)
-            val_loss[i / record_iter] = np.average(cur_val_loss)
-            val_acc[i / record_iter] = np.average(cur_val_acc)
+                        cur_train_loss = np.zeros(record_iter)
+                        cur_train_acc = np.zeros(record_iter)
+                        cur_val_loss = np.zeros(record_iter)
+                        cur_val_acc = np.zeros(record_iter)
 
-            cur_train_loss = np.zeros(record_iter)
-            cur_train_acc = np.zeros(record_iter)
-            cur_val_loss = np.zeros(record_iter)
-            cur_val_acc = np.zeros(record_iter)
-
-            train_temp_loss = train_loss[0 : i / record_iter + 1]
-            train_temp_acc = train_acc[0 : i / record_iter + 1]
-            val_temp_loss = val_loss[0 : i / record_iter + 1]
-            val_temp_acc = val_acc[0 : i / record_iter + 1]
-            np.savetxt(train_loss_path, train_temp_loss)
-            np.savetxt(train_acc_path, train_temp_acc)
-            np.savetxt(val_loss_path, val_temp_loss)
-            np.savetxt(val_acc_path, val_temp_acc)
-        #if early_stop and (i % test_iter == 0):
-        #    if i == 0:
-        #        pre_train_loss = cur_train_loss
-        #        pre_train_acc = cur_train_acc
-        #        pre_val_loss = cur_val_loss
-        #        pre_val_acc = cur_val_acc
-        #    elif (pre_train_loss - cur_train_loss) / pre_train_loss < threshold:
-        #        print "Converged, stopping...."
+                        train_temp_loss = train_loss[0 : i / record_iter + 1]
+                        train_temp_acc = train_acc[0 : i / record_iter + 1]
+                        val_temp_loss = val_loss[0 : i / record_iter + 1]
+                        val_temp_acc = val_acc[0 : i / record_iter + 1]
+                        np.savetxt(train_loss_path, train_temp_loss)
+                        np.savetxt(train_acc_path, train_temp_acc)
+                        np.savetxt(val_loss_path, val_temp_loss)
+                        np.savetxt(val_acc_path, val_temp_acc)
+                        if epoch - 1 == e:
+                           train_output = solver.net.blobs[output_layer].data
+                           train_labels = solver.net.blobs["label"].data
+                           print 'data.shape ', train_output.shape
+                           print 'label.shape ', train_labels.shape
+                           num = train_output.shape[0] 
+                           for b in range(num):
+                               data = train_output[b, :, :, :]
+                               label = int(train_labels[b])
+                               #print "data.type ", type(data[1][1][1])
+                               #print "label.type ", type(label)
+                               datum = caffe.io.array_to_datum(data, label)
+                               keystr = '0>12d'.format(b + i * batch_size)
+                               train_output_txn.put(keystr, datum.SerializeToString())
+                           if max_iter -1 == i:
+                               test_output = solver.test_nets[0].blobs[output_layer].data
+                               test_labels = solver.test_nets[0].blobs["label"].data
+                               num = test_output.shape[0]
+                               for b in range(num):
+                                   data = test_output[b, :, :, :]
+                                   label = int(test_labels[b])
+                                   datum = caffe.io.array_to_datum(data, label)
+                                   keystr = '0>12d'.format(b + i * batch_size)
+                                   test_output_txn.put(keystr, datum.SerializeToString())
+        test_output_lmdb.close()
+    train_output_lmdb.close()
+                    #if early_stop and (i % test_iter == 0):
+                    #    if i == 0:
+                    #        pre_train_loss = cur_train_loss
+                    #        pre_train_acc = cur_train_acc
+                    #        pre_val_loss = cur_val_loss
+                    #        pre_val_acc = cur_val_acc
+                    #    elif (pre_train_loss - cur_train_loss) / pre_train_loss < threshold:
+                    #        print "Converged, stopping...."
     solver.snapshot()
 
 if __name__ == "__main__":
