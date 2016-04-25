f55658466f13f5959c6faf3154c7f87fafcddf5d
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-26 00:04:08 +0800

    save output

--- src/solve_rn_cifar.sh
+++ src/solve_rn_cifar.sh
@@ -6,4 +6,4 @@ mkdir -p results/loss/ResNet-cifar
 mkdir -p results/accuracy/ResNet-cifar
 mkdir -p results/snapshots/ResNet-cifar
 
-src/solve_network.py prototxt/ResNet-cifar-solver.prototxt --train-loss results/loss/ResNet-cifar/ResNet-cifar-train-loss --train-acc results/accuracy/ResNet-cifar/ResNet-cifar-train-acc --val-loss results/loss/ResNet-cifar/ResNet-cifar-val-loss --val-acc results/accuracy/ResNet-cifar/ResNet-cifar-val-acc --max_iter 60000
+src/solve_network.py prototxt/ResNet-cifar-solver.prototxt --train-loss results/loss/ResNet-cifar/ResNet-cifar-train-loss --train-acc results/accuracy/ResNet-cifar/ResNet-cifar-train-acc --val-loss results/loss/ResNet-cifar/ResNet-cifar-val-loss --val-acc results/accuracy/ResNet-cifar/ResNet-cifar-val-acc --output_layer conv1 --batch_size 200 --max_iter 250 --epoch 80 data/ResNet-cifar/ResNet-cifar_train_lmdb data/ResNet-cifar/ResNet-cifar_test_lmdb
